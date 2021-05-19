1d819159156c2e27b53c480c962f1a6c10653a86
xiaomifeng <tianyashuibin@163.com>
2021-05-20 00:24:30 +0800

    update

---
+++ src/main/java/proxy/aop/BrokerAspect.java
@@ -0,0 +1,11 @@
+package proxy.aop;
+
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.Aspect;
+
+@Component
+@Aspect
+public class BrokerAspect {
+
+}

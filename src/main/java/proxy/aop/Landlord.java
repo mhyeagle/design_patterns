f27a7deb8f3a2aecd3d48a8bd5430f1e354e64cd
xiaomifeng <tianyashuibin@163.com>
2021-05-20 09:24:24 +0800

    update aop

--- src/main/java/proxy/aop/Landlord.java
+++ src/main/java/proxy/aop/Landlord.java
@@ -2,7 +2,7 @@ package proxy.aop;
 
 import org.springframework.stereotype.Component;
 
-@Component("landlord")
+@Component
 public class Landlord {
     public void bussiness() {
         System.out.println("签合同");
--- src/main/java/proxy/aop/Main.java
+++ src/main/java/proxy/aop/Main.java
@@ -1,12 +1,18 @@
 package proxy.aop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
+//    @Autowired
+//    private static Landlord landlordAuto;
+
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         Landlord landlord = (Landlord) context.getBean("landlord");
         landlord.bussiness();
+
+//        landlordAuto.bussiness();
     }
 }
